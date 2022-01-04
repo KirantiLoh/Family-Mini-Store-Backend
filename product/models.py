291c939f2f348525bc3a108fc42094236cb5b027
Maurice Yang <mauriceyangmy28@gmail.com>
2022-01-04 16:38:30 +0700

    Edited models.py

--- product/models.py
+++ product/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    slug = models.SlugField(null=False, unique = True)
 
     class Meta:
         verbose_name_plural = "Categories"
     
     def __str__(self):
         return f"{self.name}"
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
